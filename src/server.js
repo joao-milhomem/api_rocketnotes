2e1991e4f4c606df3e6056e5dfdff20ab50d5619
João Paulo Gomes Milhomem <joao.milhomem@grupoimne.local>
2023-01-26 15:19:16 -0300

    Implementado tratamento de excessões

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+const AppError = require("./utils/AppError");
+
 const express = require("express");
 const app = express();
 const PORT = 3333;
@@ -7,5 +10,23 @@ app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT}`)
 );
 
+
 app.use(express.json());
 app.use(appRouter);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal server error",
+  });
+
+});
