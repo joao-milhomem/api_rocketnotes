06eeab5b57bd043003db505f2259d8b179fa795a
João Paulo Gomes Milhomem <joao.milhomem@grupoimne.local>
2023-01-26 14:27:56 -0300

    Iniciando controladores

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const usersRoutes = Router();
 
-usersRoutes.post("/", (request, response) => {
-  const { name, email, password } = request.body;
-  response.json({ name, email, password });
-});
+const UsersController = require("../controllers/UsersController");
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
