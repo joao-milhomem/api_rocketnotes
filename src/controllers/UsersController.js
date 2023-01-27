a30a5d775fc006a2bbf70ee66e3a9f1f370aea1a
João Paulo Gomes Milhomem <joao.milhomem@grupoimne.local>
2023-01-27 15:13:41 -0300

    Insert no DB e bcrypt

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,30 @@
+const { hash } = require("bcrypt");
+const sqliteConnection = require("../database/sqlite");
+const AppError = require("../utils/AppError");
+
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
-    response.json({ name, email, password });
+
+    const database = await sqliteConnection();
+    const checkIfUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkIfUserExists) {
+      throw new AppError("Email já está em uso");
+    }
+
+    const encryptedPassword = await hash(password, 8);
+
+    database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)", [
+      name,
+      email,
+      encryptedPassword,
+    ]);
+
+    return response.status(201).json({});
   }
 }
 
