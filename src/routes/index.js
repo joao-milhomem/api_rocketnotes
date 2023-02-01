1a32f771bc501c801ee75285004867923238792a
João Paulo Gomes Milhomem <joao.milhomem@grupoimne.local>
2023-02-01 07:48:47 -0300

    routes e controllers finalizados

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const appRouter = Router();
 const usersRoutes = require("./users.routes");
+const notesRoutes = require("./notes.routes");
+const tagsRoutes= require("./tags.routes");
 
 appRouter.use("/users", usersRoutes);
+appRouter.use("/notes", notesRoutes);
+appRouter.use("/tags", tagsRoutes);
 
 module.exports = appRouter;
